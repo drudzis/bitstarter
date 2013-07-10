9f6960bb70ffac86854f6d418c3ebc96d68637fe
Roger Toma <rogertoma@yahoo.com>
2013-07-10 18:56:26 +0000

    updating web file

--- web.js
+++ web.js
@@ -2,17 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
 
-    var indexString = fs.readFile ('index.html', function (err, data) {
-	if (err) throw err;
-	console.log(data);
-	});
-
-  response.send(indexString);
+    var fs = require('fs');
+    var indexString = new Buffer(fs.readFileSync("index.html", "utf-8"));
+    
+    response.send(indexString.toString());
 });
+
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
+

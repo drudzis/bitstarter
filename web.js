25d6ecd06ff93640738d493f76bfc20df581f328
Roger Toma <rogertoma@yahoo.com>
2013-07-10 18:42:51 +0000

    updated to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+    var indexString = fs.readFile ('index.html', function (err, data) {
+	if (err) throw err;
+	console.log(data);
+	});
+
+  response.send(indexString);
 });
 
 var port = process.env.PORT || 5000;
